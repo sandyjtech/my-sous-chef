c6e43947443a6667504219d1b81b70024b19690a
arbitrary-berry <alyssa.brooksong@gmail.com>
2023-06-27 15:47:43 -0700

    updated from Main

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Global variables
-const enabledDietary = []
+const enabledDietary = [];
 
 //Fetch Request and Function
-const myApiKey= process.env.RAPID_API_KEY;
+//const myApiKey= process.env.RAPID_API_KEY;
 const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
 const options = {
 	method: 'GET',
@@ -11,16 +11,6 @@ const options = {
 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
 	}
 };
-async function getRecipes() {
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
-getRecipes()
 
 //DOM Elements
 const container = document.querySelector("#container")
@@ -30,37 +20,62 @@ const vegan = document.querySelector('#64468')
 const glutenFree = document.querySelector('#64465')
 const dairyFree = document.querySelector('#64463')
 const form = document.querySelector('#dietary-form')
-const closedBook = document.querySelector('.book')
-const checkboxes = document.querySelectorAll('input[type=checkbox])') 
+//const book = document.querySelector('.book')
+const checkboxes = document.querySelectorAll('input[type="checkbox"]') //added "" to checkbox
+//Create Global Elements//create recipe container elements
+let img = document.createElement("img");
+let receipe = document.createElement("h2");
+let receipeName = document.createElement("h3");
+let ingredientsList = document.createElement("ul");
+let instructions = document.createElement("h4")
+let instructionsName = document.createElement("p")
+	let eachIngredient = document.createElement("li");
+let ingredient = document.createElement("h4");
 
 //select all checkboxes with the type=checkbox 
 
 //Render Function
-
+function renderRecipes(recipesArray){
+	console.log(recipesArray);
+	recipesArray.forEach(displayRecipe);
+}
 //Event Listeners
 
-//need to change hover to an event listener vs in CSS
-closedBook.addEventListener('click', openRecipe)//not sure if that's right?
-
+//need to change hover to an event listener in CSS
+//closedBook.addEventListener('click', renderRecipes)//not sure if that's right?
+book.addEventListener('click', renderRecipes)
 
 
 //Event Handlers
-function openRecipe() {
-	e.preventDefault()
-	// remove hidden class
-	form.classList.remove("dietary-form"); //wait don't we want to have it hidden first? and the reveal  when they click the book?
-	//create recipe container elements
-	let img = document.createElement("img");
-	let recipeHeader = document.createElement("h2");
-	let ingredientsList = document.createElement("ul");
-	let ingredientsTitle = document.createElement("h3")
-    let eachIngredient = document.createElement("li");
+function displayRecipe(recipeObject) {
 
-	ingredientsTitle.appendChild()
-	recipeContainer.appendChild()
+	// remove hidden classs
+	form.classList.remove("dietary-form");
+	
+	//Assign each elements to variables
+	img.src = "./chef.png";
+	img.alt = "chef";
+	receipe.textContent = "Recipe";
+	receipeName.textContent = recipeObject.name;
+	eachIngredient.textContent = "Ingredients";
+	eachIngredient.textContent = "Ingredients are blaaaaaaa";
+	instructions.textContent = "Instructions";
+	instructionsName.textContent = "Instructions are blaaaaaaa";
+
+	//Append elements to container
+
+
+	recipeContainer.appendChild(receipe)
+	recipeContainer.appendChild(img)
+	recipeContainer.appendChild(receipeName)
+
+	ingredientsList.appendChild(eachIngredient);
+	recipeContainer.appendChild(ingredientsList)
+	recipeContainer.appendChild(eachIngredient)
+	recipeContainer.appendChild(instructions)
+	recipeContainer.appendChild(instructionsName)
 
 }
-
 //After page loads
 checkboxes.forEach(function(checkbox) { //adding an event listener to each checkbox
 	checkbox.addEventListener('change', function() {
@@ -73,5 +88,35 @@ checkboxes.forEach(function(checkbox) { //adding an event listener to each check
 
 
 //Initializers
+async function getRecipes() {
+	try {
+	  const response = await fetch(url, options);
+	  const responseData = await response.json(); // Parse response as JSON
+	  const recipesArray = responseData.results; // Extract the 'results' array from the response data
+	  renderRecipes(recipesArray);
+	  console.log(recipesArray);
+	} catch (error) {
+	  console.error(error);
+	}
+  }
 
-
+/*
+  <form id="dietary-form">
+ 	<label>  
+ 		<input type="checkbox" id="64469" name="vegetarian" value="Vegetarian">
+		Vegetarian
+	</label>
+	<label> 
+    	<input type="checkbox" id="64468 " name="vegan" value="Vegan">
+		Vegan
+	</label>
+	<label> 
+    	<input type="checkbox" id="64465 " name="gluten_free" value="Gluten-Free">
+		Gluten-Free
+	</label>
+	<label> 
+    	<input type="checkbox" id="64463 " name="dairy_free" value="Dairy-Free">
+		Dairy-Free
+	</label>    
+  </form>
+  */
