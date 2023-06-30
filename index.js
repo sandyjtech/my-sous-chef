f2bfce6c46ab274d7892be222ea6938be80eaea4
arbitrary-berry <alyssa.brooksong@gmail.com>
2023-06-30 09:35:57 -0700

    README completed

--- index.js
+++ index.js
@@ -1,136 +1,157 @@
-//Global variables
-const enabledDietary = [];
-
 //Fetch Request and Function
-//const myApiKey= process.env.RAPID_API_KEY;
-const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=1&tags=under_30_minutes';
+const url =
+  "https://tasty.p.rapidapi.com/recipes/list?from=0&size=10000&tags=dinner";
+let recipe = [];
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6ed85be156msh4a0c085492b2a53p13becejsn67c610670df6',
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bd23b1e9demsh0080b8f130a2663p13493ajsnb15e39575b85",
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+  },
 };
 
 //DOM Elements
-const container = document.getElementById("container")
+const container = document.getElementById("container");
 const recipeContainer = document.getElementById("recipe-container");
-const book = document.querySelector('.book')
-const reviewForm = document.querySelector('#review-form')
-//const checkboxes = document.querySelectorAll('input[type="checkbox"]') 
+const book = document.querySelector(".book");
+const reviewForm = document.querySelector("#review-form");
 
 //Create Global Elements//create recipe container elements
-	let img = document.createElement("img");
-	img.setAttribute("id", "recipe-img");
-	let receipe = document.createElement("h2");
-	let receipeName = document.createElement("h3");
-	let ingredientsList = document.createElement("ul");
-	let instructions = document.createElement("h3")
-	let instructionsName = document.createElement("p")
-    let eachIngredient = document.createElement("li");
-	let ingredient = document.createElement("h4");
-
-
-
+let img = document.createElement("img");
+img.setAttribute("id", "recipe-img");
+let receipe = document.createElement("h2");
+let receipeName = document.createElement("h3");
+let ingredientsList = document.createElement("ul");
+let instructions = document.createElement("h4");
+let instructionsName = document.createElement("p");
+let instructionContainer = document.createElement("div");
+instructionContainer.setAttribute("id", "instruction-container");
+let randomRecipeButton = document.createElement("button");
+let video = document.createElement("video")
+//video.setAttribute('id', )
+randomRecipeButton.setAttribute("id", "random-button");
+let reviewDiv = document.querySelector("#review-container");
+let starSpan = document.querySelectorAll(".fa")
 //Event Listeners
+book.addEventListener("click", function () {
+  openBook();
+  getRecipes();
+});
+book.addEventListener("mouseover", function () {
+  book.style.transform = "scale(1.2)";
+});
+book.addEventListener("mouseout", function () {
+  book.style.transform = "scale(1)";
+});
+book.addEventListener("click", getRecipes);
 
 
-book.addEventListener('click', function() {
-	openBook();
-	getRecipes();
-})
-book.addEventListener("mouseover", function() {
-	book.style.transform = "scale(1.2)"
-})//ADDED
-book.addEventListener("mouseout", function() {
-	book.style.transform = "scale(1)"
-})//ADDED
-
-//Render Function
+// Render Function
 function renderRecipes(recipesArray) {
-	recipesArray.forEach(displayRecipe)
+  recipe = recipesArray; // Assign the recipesArray to the global recipe variable
+  recipesArray.forEach(displayRecipe);
 }
+
 //Event Handlers
 function openBook() {
-    book.classList.add("open")
-	//book.toggle("clicked")
-  }
-
-function displayRecipe(recipesArray) {			
-	//Assign each elements to variables
-	img.src = recipesArray.thumbnail_url;
-	img.alt = recipesArray.name;
-	receipe.textContent = "Recipe";
-	receipeName.textContent = recipesArray.name;
-	ingredientsList.textContent = "Ingredients";
-	eachIngredient.textContent = "Ingredients are blaaaaaaa"; //where are the ingredients
-	instructions.textContent = "Instructions";
-	//loop through instructions
-	// for (let i = 0; i < recipesArray.instructions.length; i++) {
-    //     instructionsName.textContent = recipesArray.instructions[i].display_text
-    // }
-    // //loop through ingredients
-    // for (let i = 0; i < recipesArray.ingredients.length; i++) {
-    //     ingredient.textContent = recipesArray.ingredients[i].text
-    // }
-	//ask Lantz is this what works? 
-
-	instructionsName.textContent = recipesArray.instructions[0].display_text + 
-	recipesArray.instructions[1].display_text + 
-	recipesArray.instructions[2].display_text + 
-	recipesArray.instructions[3].display_text
-
-	//Append elements to container
-
-
-	recipeContainer.appendChild(receipe)
-	recipeContainer.appendChild(img)
-	recipeContainer.appendChild(receipeName)
-
-	ingredientsList.appendChild(eachIngredient);
-	recipeContainer.appendChild(ingredientsList)
-	recipeContainer.appendChild(eachIngredient)
-	recipeContainer.appendChild(instructions)
-	recipeContainer.appendChild(instructionsName)
-
+  book.classList.add("open");
 }
+
+function displayRecipe(recipe) {
+  reviewForm.classList.remove("hidden");  
+ 
+  ingredientsList.textContent = "Ingredients";
+  recipe.sections[0].components.forEach((component) => {
+	let eachIngredient = document.createElement("li");
+    eachIngredient.textContent = component.raw_text;
+	ingredientsList.appendChild(eachIngredient);  
+  });
+  //Assign each elements to variables
+  img.src = recipe.thumbnail_url;
+  img.alt = recipe.name;
+  receipeName.textContent = recipe.name;
+ 
+  instructions.textContent = "Instructions";
+  instructionsName.textContent = recipe.instructions[0].display_text +
+  recipe.instructions[1].display_text +
+  recipe.instructions[2].display_text +
+  recipe.instructions[3].display_text
+
+  //Append elements to container
+  recipeContainer.appendChild(receipe);
+  recipeContainer.appendChild(img);
+  recipeContainer.appendChild(receipeName);
+  instructionContainer.appendChild(instructions);
+  instructionContainer.appendChild(instructionsName);
+  recipeContainer.appendChild(ingredientsList);
+  recipeContainer.appendChild(instructionContainer);
+  randomRecipeButton.textContent = "Another Recipe";
+  recipeContainer.appendChild(randomRecipeButton);
+  return (randomIndex = Math.floor(Math.random() * recipe.length));
+}
+randomRecipeButton.addEventListener("click", generateRandomRecipe);
+
+function generateRandomRecipe() {
+  const randomIndex = Math.floor(Math.random() * recipe.length);
+  const randomRecipe = recipe[randomIndex];
+  displayRecipe(randomRecipe);
+}
+
 function submitReview() {
-	reviewForm.addEventListener('submit', function(e) {
-		e.preventDefault();
-		console.log(e.target)
-	}); 
+  reviewForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const newReview = event.target.review.value;
+    //Create Review Section
+    let reviewCard = document.createElement("div");
+    reviewCard.classList.add("review-card");
+    let reviewName = document.createElement("h3");
+    let review = document.createElement("h5");
+    reviewName.innerText = name;
+    review.innerText = newReview;
+    reviewCard.appendChild(reviewName);
+    reviewCard.append(review);
+    reviewDiv.append(reviewCard);
+	
+    //	console.log(`name: ${name}, review: ${newReview}`)
+    reviewForm.reset();
+  });
+
+
+  //add starts to reviews
+starSpan.forEach((star) => {
+	star.addEventListener("click", function () {
+        star.classList.add("checked");
+		console.log(star.classList.contains("checked"));
+	});
+		if (star.classList.contains("checked")){
+			let reviewStar = document.createElement("i");
+			reviewStar.classList.add("fa");
+			reviewStar.classList.add("fa-star");
+			reviewStar.classList.add("checked");
+			reviewDiv.appendChild(reviewStar);
+		
+		}
+
+});
 }
-//After page loads
-// checkboxes.forEach(function(checkbox) { //adding an event listener to each checkbox
-// 	checkbox.addEventListener('change', function(e) {
-// 		enabledDietary => { 
-// 			Array.from(checkboxes) //converts checkboxes to an array to use filter and map
-// 			.filter(i => i.checked) //Array.filter to removed unchecked checkboxes
-// 			.map(i => i.value) // Array.map to extract only the checkbox values from the array
-// }
-//         if (e.target.value === "vegetarian") {
-			
-// 		})
-// }) //we don't want prevent default do we? we want the page to refresh with the newly selected dietary choice
 
 
 //Initializers
 async function getRecipes() {
-	try {
-	  const response = await fetch(url, options);
-	  const responseData = await response.json(); // Parse response as JSON
-	  const recipesArray = responseData.results; // Extract the 'results' array from the response data
-	//.mapFilter random something
-	  renderRecipes(recipesArray);
-	//displayRecipe(recipesArray)
-	  console.log(recipesArray);
-	} catch (error) {
-	  console.error(error);
-	}
-  }  
-  //getRecipes()
+  try {
+    const response = await fetch(url, options);
+    const responseData = await response.json(); // Parse response as JSON
+    const recipesArray = responseData.results;
+    // Extract the 'results' array from the response data
+    console.log(recipesArray);
+    renderRecipes(recipesArray);
+  } catch (error) {
+    console.error(error);
+  }
+}
+submitReview();
 
-
-  //total_time_minutes, 
-  //yields serviens size
-  //video_url
+//total_time_minutes,
+//yields servings size
+//video_url
